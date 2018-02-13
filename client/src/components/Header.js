d8968342a8cbb290d0ce29e603bb7fc57d828042
Gus <augustin1989@gmail.com>
2018-02-13 18:58:12 -0500

    Added billing and client side app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
         switch (this.props.auth) {
             case null:
-                return 'Still deciding';
+                return;
             case false:
-                return 'Im logged out';
+                return <li><a href="/auth/google">Login With Google</a></li>;
             default:
-                return 'Im logged in';
+                return [
+                    <li key="1"><Payments/></li>,
+                    <li key="3" style={{ margin: '0 10px' }}>
+                        Credits: { this.props.auth.credits}
+                    </li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
 
@@ -17,9 +25,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className="left brand-logo"
+                    >
                         Emaily
-                    </a>
+                    </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
